6f682dfc7d568e8ae48140e610f75a41c35dc2c5
Vilperi27 <vilppu_97@hotmail.com>
2020-02-17 12:52:09 +0200

    Made changes to UI and SQL users fetching

---
+++ Assets/FillUsers.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FillUsers : MonoBehaviour {
+
+    public GameObject userObject;
+    public GameObject content;
+    private GameObject user;
+    private UserProfile up;
+    public GameObject pf;
+
+	// Use this for initialization
+	void Start () {
+        pf = GameObject.Find("PinField");
+        StartCoroutine(GetUsers());
+    }
+
+    IEnumerator GetUsers()
+    {
+        WWWForm form = new WWWForm();
+
+        WWW www = new WWW("http://localhost/sqlconnect/getusers.php");
+
+        user = Instantiate(userObject, content.transform);
+        up = user.GetComponent<UserProfile>();
+        pf.SetActive(false);
+        yield return www;
+    }
+}
