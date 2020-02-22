19edc13afaf25807e58ec571aa6c9507b66611a1
joel <hehexd@hotmail.com>
2020-02-22 15:40:00 +0200

    LoadScene scenen vaihtoa varten ja TEST vain printtaa konsoliin minkä napin valitsee ValitsePituus scenessä

---
+++ Assets/Scripts/TEST.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TEST : MonoBehaviour
+{
+    public void OnClicked(Button button) 
+    {
+        print ("Videon pituus: " + button.name);
+    }
+}
